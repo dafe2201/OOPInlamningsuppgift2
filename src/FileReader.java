dc82910b8c8b25fb6f04f64e69477e866e0e8180
dafe2201 <daniel_fellden@hotmail.com>
2022-10-19 23:40:14 +0200

    General clean-up

--- src/FileReader.java
+++ src/FileReader.java
@@ -20,9 +20,9 @@ public class FileReader {
                 sb.append(line.trim()).append(", ");
                 counter++;
                 if (counter >= 2) {
-                    // Tar bort komma
+                    // Tar bort sista komma
                     sb.replace(sb.length() - 2, sb.length() - 1, "");
-                    // Tar bort sista "," för att kunna lägga till i array.
+                    // delar upp till tre lines till array med split.
                     String[] currentLine = sb.toString().trim().split(", ");
                     // Skapar och lägger till Customer i lista customers.
                     customers.add(new Customer(currentLine[0], currentLine[1], LocalDate.parse(currentLine[2])));
